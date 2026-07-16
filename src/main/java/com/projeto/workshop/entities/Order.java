3eca7151120028b5f2390342d3f875542495b4a5
EmanuelScapim <emanuelrrscapim@gmail.com>
2026-07-16 19:45:35 -0300

    feat: Subtotal & Total methods

--- src/main/java/com/projeto/workshop/entities/Order.java
+++ src/main/java/com/projeto/workshop/entities/Order.java
@@ -1,5 +1,6 @@
 package com.projeto.workshop.entities;
 
+
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.HashSet;
@@ -101,6 +102,14 @@ public class Order implements Serializable {
 
 	public void setPayment(Payment payment) {
 		this.payment = payment;
+	}
+	
+	public Double getTotal() {
+		double sum = 0;
+		for(OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
 	}
 
 	@Override
